c2e2741ba47453856e6160ba6ea4d030e30685ab
AgosBC <agostinabarello@gmail.com>
2021-07-22 15:36:54 -0300

    creacion de reserva repository

---
+++ src/main/java/ar/com/ada/api/aladas/repos/ReservaRepository.java
@@ -0,0 +1,11 @@
+package ar.com.ada.api.aladas.repos;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ar.com.ada.api.aladas.entities.Reserva;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Integer>{
+    
+}
