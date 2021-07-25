08e0fa7fd08cb8ffebd8eb21175484c71ff6b42e
AgosBC <agostinabarello@gmail.com>
2021-07-25 13:48:55 -0300

    add aeropuerto repo, conroller and service empty

---
+++ src/main/java/ar/com/ada/api/aladas/controllers/AeropuertoController.java
@@ -0,0 +1,12 @@
+package ar.com.ada.api.aladas.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.aladas.services.AeropuertoService;
+
+@RestController
+public class AeropuertoController {
+
+    public AeropuertoService service;
+    
+}
---
+++ src/main/java/ar/com/ada/api/aladas/services/AeropuertoService.java
@@ -0,0 +1,12 @@
+package ar.com.ada.api.aladas.services;
+
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.aladas.repos.AeropuertoRepository;
+
+@Service
+public class AeropuertoService {
+
+    private AeropuertoRepository repo;
+    
+}
